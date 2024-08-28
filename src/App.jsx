741c2ae51b65a1c192ea9dbc9e71144089be8d0a
sami5671 <samialam5671@gmail.com>
2024-08-28 22:32:51 +0600

    wow

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
       <div>
-        <h1 className='bg-primary  text-light-yellow'>LandSync</h1>
+        <h1 className="text-primary">LandSync</h1>
+        <p>wo wo</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
